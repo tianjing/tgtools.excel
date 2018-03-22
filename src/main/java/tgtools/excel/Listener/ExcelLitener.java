d2c3b29947292aef9bc5710ce6f72ae4c6ae6529
田径 <tian___jing@hotmail.com>
2018-03-22 17:14:20 +0800

    更新忽略文件

---
+++ src/main/java/tgtools/excel/Listener/ExcelLitener.java
@@ -0,0 +1,31 @@
+package tgtools.excel.Listener;
+
+import jxl.Cell;
+import tgtools.data.DataColumn;
+import tgtools.data.DataRow;
+
+/**
+ * Created by tian_ on 2016-06-28.
+ */
+public interface ExcelLitener {
+
+    /**
+     * 添加列后事件
+     * @param p_Column 列对象
+     * @param p_Cell excel的单元格对象
+     * @param p_ColIndex 列索引
+     */
+    void onAddColumn(DataColumn p_Column, Cell p_Cell, int p_ColIndex);
+
+    /**
+     * 添加值后事件
+     * @param p_Row 行对象
+     * @param p_Cell excel的单元格对象
+     * @param p_RowIndex 行索引
+     * @param p_ColIndex 列索引
+     */
+    void onAddValue(DataRow p_Row, Cell p_Cell, int p_RowIndex, int p_ColIndex);
+
+
+
+}
